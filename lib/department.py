68232c283efc2b0038445221c523c41ae28fa324
emmanuel <emmanuelhongo5@gmail.com>
2025-11-25 20:15:26 +0300

    lab work

--- lib/department.py
+++ lib/department.py
@@ -1,5 +1,4 @@
-from __init__ import CURSOR, CONN
-
+from __init__ import CONN, CURSOR
 
 class Department:
 
@@ -8,5 +7,51 @@ class Department:
         self.name = name
         self.location = location
 
-    def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = "DROP TABLE IF EXISTS departments"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+
+        # Assign the id AFTER save
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
